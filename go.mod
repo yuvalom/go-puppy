4e0830955c9cbe41d56556c911ad96f911d3a56b
yuvalom <92787451+yuvalom@users.noreply.github.com>
2024-01-31 15:08:09 +0200

    Big bark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuvalom/go-puppy
 
 go 1.21.3
+
+require github.com/yuvalom/go-dog v0.0.0-20240131073202-529cba7ba225 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package gopuppy
 
+import (
+	godog "github.com/yuvalom/go-dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return godog.WhenGrownUp("Woof!")
 }
