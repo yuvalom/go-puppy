4fd30620cf5c60aa20418dda9d7c8a3458426453
yuvalom <92787451+yuvalom@users.noreply.github.com>
2024-01-31 15:57:26 +0200

    1.1.0

--- puppy.go
+++ puppy.go
@@ -19,3 +19,7 @@ func BigBark() string {
 func BigBarks() string {
 	return godog.WhenGrownUp("Woof! Woof! Woof!")
 }
+
+func From1_1_0() string {
+	return godog.WhenGrownUp("I am from version 1.1.0")
+}
